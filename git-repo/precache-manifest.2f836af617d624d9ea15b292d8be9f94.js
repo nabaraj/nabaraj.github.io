0601f62719be8df816f903ccc7898222dd6fab05
Nabaraj saha <nabaraj2k@gmail.com>
2020-03-07 00:01:01 +0530

    first commit

---
+++ git-repo/precache-manifest.2f836af617d624d9ea15b292d8be9f94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f67029ec483e6167b27864ed6b4d61",
+    "url": "/git-repo/index.html"
+  },
+  {
+    "revision": "e6f61383873dbf545143",
+    "url": "/git-repo/static/css/main.c8cbfe6e.chunk.css"
+  },
+  {
+    "revision": "72cfc6260f74e68a3eaa",
+    "url": "/git-repo/static/js/2.c4ab621c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/git-repo/static/js/2.c4ab621c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6f61383873dbf545143",
+    "url": "/git-repo/static/js/main.96efda0d.chunk.js"
+  },
+  {
+    "revision": "46d1e3db7c600c8b20f2",
+    "url": "/git-repo/static/js/runtime-main.b61db264.js"
+  }
+]);
