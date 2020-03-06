2103d8c17fe2ac4d100fc59723b00ed52711adbf
Nabaraj saha <nabaraj2k@gmail.com>
2020-03-07 00:04:30 +0530

    modified host

---
+++ git-repo/precache-manifest.bd15d68e0425c3e84ed08aac793e74a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63179ac3c1a67a3efbb164cf14ed0256",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c143d66ecbb570982ad1",
+    "url": "/static/css/main.c8cbfe6e.chunk.css"
+  },
+  {
+    "revision": "72cfc6260f74e68a3eaa",
+    "url": "/static/js/2.c4ab621c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.c4ab621c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c143d66ecbb570982ad1",
+    "url": "/static/js/main.f9c83f8b.chunk.js"
+  },
+  {
+    "revision": "776e9f763a78d0705552",
+    "url": "/static/js/runtime-main.42609830.js"
+  }
+]);
