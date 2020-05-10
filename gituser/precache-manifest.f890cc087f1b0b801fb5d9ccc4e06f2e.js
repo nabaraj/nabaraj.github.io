07963373e2bbf9c024d9164746a8faf0c24866d8
Nabaraj saha <nabaraj2k@gmail.com>
2020-05-10 13:50:24 +0530

    updated build folder

---
+++ gituser/precache-manifest.f890cc087f1b0b801fb5d9ccc4e06f2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0aa83babb62523548f5a021881ca0a1",
+    "url": "/gituser/index.html"
+  },
+  {
+    "revision": "1819df5e2653f714a369",
+    "url": "/gituser/static/css/main.0082b993.chunk.css"
+  },
+  {
+    "revision": "abd2af832b930cdeda4f",
+    "url": "/gituser/static/js/2.e41fa0a0.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/gituser/static/js/2.e41fa0a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1819df5e2653f714a369",
+    "url": "/gituser/static/js/main.62a0dc11.chunk.js"
+  },
+  {
+    "revision": "574711f301588ba34905",
+    "url": "/gituser/static/js/runtime-main.6de92d04.js"
+  }
+]);
--- gituser/service-worker.js
+++ gituser/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0794681fdb8592c9773f21e38d5f7f7c.js"
+  "/gituser/precache-manifest.f890cc087f1b0b801fb5d9ccc4e06f2e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gituser/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
