03c94a8ea327bf19e24bd9bbbe9f11053f286b36
Nabaraj saha <nabaraj2k@gmail.com>
2020-05-10 13:41:05 +0530

    uploaded build folder

---
+++ gituser/precache-manifest.0794681fdb8592c9773f21e38d5f7f7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa496b11110574610bde15dafc119e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3c4d3a4bc0702e618d2",
+    "url": "/static/css/main.0082b993.chunk.css"
+  },
+  {
+    "revision": "abd2af832b930cdeda4f",
+    "url": "/static/js/2.e41fa0a0.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.e41fa0a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c4d3a4bc0702e618d2",
+    "url": "/static/js/main.6ed90ccc.chunk.js"
+  },
+  {
+    "revision": "43e72427c7989b1f5a4b",
+    "url": "/static/js/runtime-main.fb45f532.js"
+  }
+]);
